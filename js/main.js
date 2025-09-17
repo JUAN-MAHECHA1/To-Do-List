05a7c7c683d2d31d0bed521fae9ee0fcd079571e
Juan Mahecha <mahechajuanda29@gmail.com>
2025-09-17 16:28:11 -0500

    cree el html y el css

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const input = document.getElementById('taskInput');
+const addBtn = document.getElementById('addTaskBtn');
+const toDoList = document.getElementById('cont-to-do-list');
+const completedList = document.getElementById('cont-completed-list');
